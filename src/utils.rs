9f5fe0138b4d8ff4e307b5c1ad2c2b7e64e657d7
Satvik Shukla <61213829+satvik-s@users.noreply.github.com>
2022-03-21 00:06:04 -0700

    style change (#3)

--- src/utils.rs
+++ src/utils.rs
@@ -21,14 +21,19 @@ pub fn get_matched_route_and_server(
     path: &str,
     config: &ProcessedConfig,
 ) -> Result<RoutingConfig, String> {
-    let found_route = config.routes.values().find(|&x| x.path == path);
-    let unwrapped_route = found_route.ok_or_else(|| String::from("no matching route"))?;
+    let found_route = config
+        .routes
+        .values()
+        .find(|&x| x.path == path)
+        .ok_or_else(|| String::from("no matching route"))?;
 
-    let found_server = config.servers.get(&unwrapped_route.server);
-    let unwrapped_server = found_server.ok_or_else(|| String::from("no matching server"))?;
+    let found_server = config
+        .servers
+        .get(&found_route.server)
+        .ok_or_else(|| String::from("no matching server"))?;
 
     return Ok(RoutingConfig {
-        route: unwrapped_route.clone(),
-        server: unwrapped_server.clone(),
+        route: found_route.clone(),
+        server: found_server.clone(),
     });
 }
